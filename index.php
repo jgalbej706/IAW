a2dd1f27f339e58f34eb21ada91e05176d55e206
jgalbej706 <147372624+jgalbej706@users.noreply.github.com>
2023-10-09 09:51:50 +0200

    Create index.php
    mi primer comit meti el codigo

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+       <?php 
+           $x = 5985; 
+           var_dump($x); 
+           $y = 8.75; 
+           var_dump($y); 
+           $z = false; 
+           var_dump($z); 
+           $w = "Hi!"; 
+           var_dump($w);
+           
+?>
+    </body>
+</html>
